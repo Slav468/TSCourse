cdcdc9bff90e2af823f2ae48d85ddc091223e388
Slav468 <55102248+Slav468@users.noreply.github.com>
2024-07-30 19:50:30 +0300

    feat: add start lessons

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
-var str = 'sdffdgfdgf';
+var str = 'str';
 console.log(str);
+// Data types
+var userId = 'Patrick'; // String
+var number = NaN; // Number infinity, 1, -1, 3e10
+var boolean = false; // Boolean true or false
+var isBirthdayData = true;
+var ageData = 36;
+var userNameData = 'John';
+// Type Any
+// Parse = any
+var userData = '{"isBirthdayData: boolean":"true", "ageData: number":"36", "userNameData: string":"John" }';
+var userObj = JSON.parse(userData);
+// let num;
+// Empty = any
+var salary;
+salary = 5000;
+//** */ Task
+// ---------------------------------------------------------------
+var currRate = '1.05';
+var fetchCurr = function (response) {
+    var data = JSON.parse(response);
+    return data;
+};
+function transferEurToUsd(available, amount, commission) {
+    if (available) {
+        var res = fetchCurr(currRate) * amount * commission;
+        console.log(res);
+    }
+    else {
+        console.log('Сейчас обмен недоступен');
+    }
+}
+transferEurToUsd(true, 500, 1.05);
+// ----------------------------------------------------------------
+// Never
+var createError = function (msg) {
+    throw new Error(msg);
+};
+function logBrtMsg(isBirthday, userName, age) {
+    if (isBirthday) {
+        return "Congrats ".concat(userName.toUpperCase(), ", ").concat(age + 1);
+    }
+    else {
+        return createError('Error');
+    }
+}
+logBrtMsg(isBirthdayData, userNameData, ageData);
+// null
+var test = null;
+var test2 = null;
+// const test11: string = null; // Error
+// const test22: number = null; // Error
+// undefined
+var test3 = undefined;
+var test4 = undefined;
+// const test33: string = undefined; // Error
+// const test44: any = undefined;// Error
--- index.ts
+++ index.ts
@@ -1,5 +1,84 @@
-const str = 'sdffdgfdgf';
+const str = 'str';
 console.log(str);
+
+// Data types
+let userId: string = 'Patrick'; // String
+let number: number = NaN; // Number infinity, 1, -1, 3e10
+let boolean: boolean = false; // Boolean true or false
+
+const isBirthdayData: boolean = true;
+const ageData: number = 36;
+const userNameData: string = 'John';
+
+// TYPE Any
+// Parse = any
+const userDataJson =
+	'{"isBirthdayData: boolean":"true", "ageData: number":"36", "userNameData: string":"John" }';
+
+const userObj: {
+	isBirthdayData: boolean;
+	ageData: number;
+	userNameData: string;
+} = JSON.parse(userDataJson);
+
+// let num;
+// Empty = any
+
+let salary;
+salary = 5_000;
+
+//! Task
+// ---------------------------------------------------------------
+const currRate = '1.05';
+
+const fetchCurr = (response: string): number => {
+	const data: number = JSON.parse(response);
+	return data;
+};
+
+function transferEurToUsd(
+	available: boolean,
+	amount: number,
+	commission: number
+): void {
+	if (available) {
+		let res: number = fetchCurr(currRate) * amount * commission;
+		console.log(res);
+	} else {
+		console.log('Сейчас обмен недоступен');
+	}
+}
+
+transferEurToUsd(true, 500, 1.05);
+// ----------------------------------------------------------------
+//! Task
+
+// TYPE Never
+const createError = (msg: string) => {
+	throw new Error(msg);
+};
+
+function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+	if (isBirthday) {
+		return `Congrats ${userName.toUpperCase()}, ${age + 1}`;
+	} else {
+		return createError('Error');
+	}
+}
+
+logBrtMsg(isBirthdayData, userNameData, ageData);
+
+// TYPE null
+const test: null = null;
+const test2: any = null;
+// const test11: string = null; // Error
+// const test22: number = null; // Error
+
+// TYPE undefined
+const test3: undefined = undefined;
+const test4: any = undefined;
+// const test33: string = undefined; // Error
+// const test44: any = undefined;// Error
 
 // TYPE Symbol
 const id1: symbol = Symbol('symbol1');
