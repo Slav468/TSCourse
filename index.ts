347eac0aaf6cfdec6e81f3e5b813ffd2eb200229
Slav468 <55102248+Slav468@users.noreply.github.com>
2024-08-10 11:51:57 +0300

    add: typing object and array

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ let salary;
 salary = 5_000;
 
 //! Task
-// ---------------------------------------------------------------
+// ---------------------------------------------------------------//
 const currRate = '1.05';
 
 const fetchCurr = (response: string): number => {
@@ -50,7 +50,7 @@ function transferEurToUsd(
 }
 
 transferEurToUsd(true, 500, 1.05);
-// ----------------------------------------------------------------
+// ----------------------------------------------------------------//
 //! Task
 
 // TYPE Never
@@ -95,30 +95,124 @@ const num2: bigint = 2n;
 console.log(num1 + num2);
 
 // TYPE Object
-
-// Not typing  in Object
+//? Not typing  in Object
 const userData = {
 	isBirthdayData: true,
 	ageData: 40,
 	userNameData: 'Jack',
+	messagesData: {
+		error: 'error',
+	},
 };
 
 // Typing object as function arguments
-
-function logBrhMsg(data: {
+//? Destructuring object
+function logBrhMsg({
+	isBirthdayData,
+	ageData,
+	userNameData,
+	messagesData: { error },
+}: {
 	isBirthdayData: boolean;
 	ageData: number;
 	userNameData: string;
+	messagesData: { error: string };
 }): string {
-	if (data.isBirthdayData) {
-		return `Congrats ${data.userNameData.toUpperCase()}, age: ${
-			userData.ageData
-		}`;
-	} else if (!data.isBirthdayData) {
+	if (isBirthdayData) {
+		return `Congrats ${userNameData.toUpperCase()}, age: ${ageData}`;
+	} else if (!isBirthdayData) {
 		return `Too bad`;
 	} else {
-		return `Error`;
+		return error;
 	}
 }
 
 logBrhMsg(userData);
+
+// Typing array
+//? string[] читается как массив содержащий стоки (состоящий из таких типов данных, могут быть другие типы данных)
+const num4: number[] = [1, 2, 3, 4];
+
+const departments: string[] = ['dev', 'design', 'marketing'];
+const department = departments[0];
+// departments.push();
+const report = departments
+	.filter((d: string) => d !== 'dev')
+	.map((d: string) => `${d} - done!`);
+
+//! ТАК ДЕЛАТЬ НЕ НУЖНО (но можно)
+const anyData: any[] = [1, 'string', false];
+
+//? number[][] читается как массив массивов (матрица)
+const num3: number[][] = [
+	[1, 2, 3],
+	[1, 2, 3],
+];
+
+// Destructuring Array
+const [dev] = report;
+console.log(dev);
+
+//! Task
+// ----------------------------------------------------------------//
+
+const electricityUserData = {
+	readings: 95,
+	units: 'kWt',
+	mode: 'double',
+};
+
+const waterUserData = {
+	readings: 3,
+	units: 'm3',
+};
+
+const elRate: number = 0.45;
+const wRate: number = 2;
+
+const monthPayments: number[] = [0, 0]; // [electricity, water]
+
+const calculatePayments = (
+	// destructuring object
+	{ readings, units, mode }: { readings: number; units: string; mode: string },
+	wData: { readings: number; units: string },
+	elRate: number,
+	wRate: number
+): void => {
+	if (mode === 'double' && readings < 50) {
+		monthPayments[0] = readings * elRate * 0.7;
+	} else {
+		monthPayments[0] = readings * elRate;
+	}
+
+	monthPayments[1] = wData.readings * wRate;
+};
+
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+
+const sendInvoice = (
+	// destructuring array
+	[el, water]: number[],
+	electricityUserData: { readings: number; units: string; mode: string },
+	waterUserData: { readings: number; units: string }
+): string => {
+	const text: string = `    Hello!
+    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+    It will cost: ${el}€
+
+    This month you used ${waterUserData.readings} ${waterUserData.units} of water
+    It will cost: ${water}€`;
+
+	return text;
+};
+
+const invoice: string = sendInvoice(
+	monthPayments,
+	electricityUserData,
+	waterUserData
+);
+
+console.log(invoice);
+
+// ----------------------------------------------------------------//
+//! Task
