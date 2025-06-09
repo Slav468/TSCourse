5190bb2754c35beb0b81c26f83e417ab93fe2c04
Sergey <55102248+Slav468@users.noreply.github.com>
2025-06-09 10:12:01 +0300

    fix: types in taskClasses

--- taskClasses.ts
+++ taskClasses.ts
@@ -1,9 +1,9 @@
 interface Queue<T> {
-	enqueue(item: T): void; // поставить в очередь
-	dequeue(): T | undefined; // исключить из очереди
-	peek(): T | undefined | null; // посмотреть первый элемент
-	isEmpty(): boolean; // проверка на "пустоту" сущности
-	length(): number; // проверка на длину
+  enqueue(item: T): void; // поставить в очередь
+  dequeue(): T | undefined; // исключить из очереди
+  peek(): T | undefined | null; // посмотреть первый элемент
+  isEmpty(): boolean; // проверка на "пустоту" сущности
+  length(): number; // проверка на длину
 }
 
 // Реализация очереди через массив
@@ -15,28 +15,28 @@ interface Queue<T> {
 // Чуть подробнее можно найти в википедии или на других сайтах по поиску "Очередь структура данных"
 
 class ArrayQueue<T> implements Queue<T> {
-	// Создать приватное свойство queue, которое по умолчанию массив и содержит массив любого типа
-	private queue: T[] = [];
-	// Подсказка по методам:
-	// при добавлении в очередь можно выполнить метод push
-	enqueue(this: ArrayQueue<T>, item: T): void {
-		this.queue.push(item);
-	}
-	dequeue(): T | undefined {
-		return !this.isEmpty() ? this.queue.shift() : undefined;
-	}
-	peek(): T | null | undefined {
-		return this.isEmpty() ? this.queue[0] : null;
-	}
-	length(): number {
-		return this.queue.length;
-	}
-	// при удалении - shift, так как нужно удалить первый элемент.
-	// Обратите внимание на возвращаемое значение
-	// isEmpty может использоваться в других методах
-	isEmpty(): boolean {
-		return this.queue.length === 0;
-	}
+  // Создать приватное свойство queue, которое по умолчанию массив и содержит массив любого типа
+  private queue: T[] = [];
+  // Подсказка по методам:
+  // при добавлении в очередь можно выполнить метод push
+  enqueue(this: ArrayQueue<T>, item: T): void {
+    this.queue.push(item);
+  }
+  dequeue(): T | undefined {
+    return !this.isEmpty() ? this.queue.shift() : undefined;
+  }
+  peek(): T | null | undefined {
+    return this.isEmpty() ? this.queue[0] : null;
+  }
+  length(): number {
+    return this.queue.length;
+  }
+  // при удалении - shift, так как нужно удалить первый элемент.
+  // Обратите внимание на возвращаемое значение
+  // isEmpty может использоваться в других методах
+  isEmpty(): boolean {
+    return this.queue.length === 0;
+  }
 }
 
 // Стэк - это еще одна структура данных. Проще всего её представить как стопку листов на столе
@@ -46,58 +46,58 @@ class ArrayQueue<T> implements Queue<T> {
 // Класс может работать с любым типом данных, то есть помещать любые данные в массив и содержит массив любого типа  <-- Важно
 
 class Stack<T> {
-	// Создать приватное свойство stack, которое по умолчанию массив и содержит массив любого типа
-	private stack: T[] = [];
-	// Создать приватное свойство limit, которое будет типом number
-	private limit: number = Number.MAX_VALUE;
-	// Здесь мы установим лимит на стопку листов.
-	// При переполнении стэка программа зависает, а очень высокая стопка листов падает
-	// Так что лимит всегда должен быть
-	constructor(limit: number = Number.MAX_VALUE) {
-		this.limit = limit;
-	}
+  // Создать приватное свойство stack, которое по умолчанию массив и содержит массив любого типа
+  private stack: T[] = [];
+  // Создать приватное свойство limit, которое будет типом number
+  private limit: number = Number.MAX_VALUE;
+  // Здесь мы установим лимит на стопку листов.
+  // При переполнении стэка программа зависает, а очень высокая стопка листов падает
+  // Так что лимит всегда должен быть
+  constructor(limit: number = Number.MAX_VALUE) {
+    this.limit = limit;
+  }
 
-	push(this: Stack<T>, value: T): void {
-		// Добавляет элемент в стэк
-		// Если стэк переполнен - выбрасывает ошибку (throw new Error)
-		if (this.length() + 1 > this.limit) {
-			throw new Error('Stack is full');
-		}
+  push(this: Stack<T>, value: T): void {
+    // Добавляет элемент в стэк
+    // Если стэк переполнен - выбрасывает ошибку (throw new Error)
+    if (this.length() + 1 > this.limit) {
+      throw new Error('Stack is full');
+    }
 
-		this.stack.push(value);
-	}
+    this.stack.push(value);
+  }
 
-	pop(): T {
-		// Удаляет последний элемент массива
-		// Если в стеке пусто - выбрасывает ошибку (throw new Error)
-		// При удалении элемента возвращает его
-		// Простыми словами: вы берете верхний лист в стопке и используете его
-		// Если на столе нет листов - получается ошибка, брать нечего
-		if (!this.length()) {
-			throw new Error('Stack is empty');
-		}
-		return this.stack.pop() as T;
-	}
+  pop(): T {
+    // Удаляет последний элемент массива
+    // Если в стеке пусто - выбрасывает ошибку (throw new Error)
+    // При удалении элемента возвращает его
+    // Простыми словами: вы берете верхний лист в стопке и используете его
+    // Если на столе нет листов - получается ошибка, брать нечего
+    if (!this.length()) {
+      throw new Error('Stack is empty');
+    }
+    return this.stack.pop() as T;
+  }
 
-	length(): number {
-		// Возвращает кол-во элементов в стэке
-		return this.stack.length;
-	}
+  length(): number {
+    // Возвращает кол-во элементов в стэке
+    return this.stack.length;
+  }
 
-	isEmpty(): boolean {
-		// Проверяет стэк на "пустоту"
-		return this.stack.length === 0;
-	}
+  isEmpty(): boolean {
+    // Проверяет стэк на "пустоту"
+    return this.stack.length === 0;
+  }
 
-	top(): T | null {
-		// Возвращает последний (верхний) элемент стэка, но не удаляет его
-		// Вы просто читаете, что написано на верхнем листе
-		// Если стэк пуст - вернется null
-		if (this.isEmpty()) {
-			return null;
-		}
-		return this.stack[this.length() - 1];
-	}
+  top(): T | undefined {
+    // Возвращает последний (верхний) элемент стэка, но не удаляет его
+    // Вы просто читаете, что написано на верхнем листе
+    // Если стэк пуст - вернется null
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.stack[this.length() - 1];
+  }
 }
 
 // Для тестов
